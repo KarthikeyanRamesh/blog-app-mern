dd17a9e6dc269f32d287e58090b076d613a35204
Karthikeyan Ramesh <karthisujatha10@gmail.com>
2024-01-08 08:44:46 +0530

    mern-blog

---
+++ client/src/Header.js
@@ -0,0 +1,47 @@
+import { useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
+
+export default function Header() {
+    const {userInfo, setUserInfo} = useContext(UserContext);
+    useEffect(() => {
+        fetch('http://localhost:4000/profile', {
+            credentials: 'include'
+        }).then(response => {
+            response.json().then(user => {
+                setUserInfo(user);
+            });
+        });
+    }, []);
+
+    function logout() {
+        fetch('http://localhost:4000/logout', {
+            credentials: 'include',
+            method: 'POST'
+        })
+        setUserInfo(null);
+    }
+
+    const username = userInfo?.username;
+
+    return (
+        <header>
+            <Link to="/" className="logo">MyBlog</Link>
+            <nav>
+                {username && (
+                    <>
+                        <Link to='/create'>Create new post</Link>
+                        <Link onClick={logout}>logout</Link>
+                    </>
+                )}
+                {!username && (
+                    <>
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                    </>
+                )}
+                
+            </nav>
+      </header>
+    )
+}
