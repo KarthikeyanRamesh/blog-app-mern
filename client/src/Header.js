eb1e095f5265075c932420e45b515fd4e1f58bc8
Karthikeyan Ramesh <karthisujatha10@gmail.com>
2024-02-17 09:22:10 +0530

    added welcome msg

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,54 +3,57 @@ import { Link } from "react-router-dom";
 import { UserContext } from "./UserContext";
 
 export default function Header() {
-    const {userInfo, setUserInfo} = useContext(UserContext);
-    useEffect(() => {
-        // fetch(`${process.env.REACT_APP_SERVERURL}/profile`, {
-        //     // credentials: 'same-origin'
-        // }).then(response => {
-        //     response.json().then(user => {
-        //         setUserInfo(user);
-        //     });
-        // });
-        const localUserInfo = localStorage.getItem("userInfo");
-        if(localUserInfo) {
-            const obj = JSON.parse(localUserInfo)
-            if(obj && obj.username) {
-                setUserInfo(obj);
-            }
-        }
-        
-    }, []);
-
-    function logout() {
-        fetch(`${process.env.REACT_APP_SERVERURL}/logout`, {
-            credentials: 'same-origin',
-            method: 'POST'
-        })
-        setUserInfo(null);
-        localStorage.setItem("userInfo", "");
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  useEffect(() => {
+    // fetch(`${process.env.REACT_APP_SERVERURL}/profile`, {
+    //     // credentials: 'same-origin'
+    // }).then(response => {
+    //     response.json().then(user => {
+    //         setUserInfo(user);
+    //     });
+    // });
+    const localUserInfo = localStorage.getItem("userInfo");
+    if (localUserInfo) {
+      const obj = JSON.parse(localUserInfo);
+      if (obj && obj.username) {
+        setUserInfo(obj);
+      }
     }
+  }, []);
 
-    const username = userInfo?.username;
+  function logout() {
+    fetch(`${process.env.REACT_APP_SERVERURL}/logout`, {
+      credentials: "same-origin",
+      method: "POST",
+    });
+    setUserInfo(null);
+    localStorage.setItem("userInfo", "");
+  }
 
-    return (
-        <header>
-            <Link to="/" className="logo">MyBlog</Link>
-            <nav>
-                {username && (
-                    <>
-                        <Link to='/create'>Create new post</Link>
-                        <Link onClick={logout}>logout</Link>
-                    </>
-                )}
-                {!username && (
-                    <>
-                        <Link to="/login">Login</Link>
-                        <Link to="/register">Register</Link>
-                    </>
-                )}
-                
-            </nav>
+  const username = userInfo?.username;
+
+  return (
+    <>
+      <h5> Welcome {username} !!!</h5>
+      <header>
+        <Link to="/" className="logo">
+          MyBlog
+        </Link>
+        <nav>
+          {username && (
+            <>
+              <Link to="/create">Create new post</Link>
+              <Link onClick={logout}>logout</Link>
+            </>
+          )}
+          {!username && (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}
+        </nav>
       </header>
-    )
+    </>
+  );
 }
